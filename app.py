ffa1c1d6000d765a0a7f026d135791d6b57364da
Tuhin Mukherjee <59216687+whitexgod@users.noreply.github.com>
2020-06-14 00:05:53 -0700

    deployment

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask,render_template,request
+import pandas as pd
+import numpy as np
+import pickle
+
+clf=pickle.load(open('model86%.pkl','rb'))
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    try:
+
+        brand=request.form.get('brand')
+        ram=request.form.get('ram')
+        rom=request.form.get('rom')
+        screen=request.form.get('screen')
+        Primary_cam=request.form.get('Primary_cam')
+        front_cam=request.form.get('front_cam')
+        screen=screen.lower().strip('inch')
+        Primary_cam=Primary_cam.lower().strip('megapixel')
+        front_cam=front_cam.lower().strip('megapixel')
+
+        new = pd.DataFrame(columns=['screen_size', 'Primary_cam', 'front_cam', 'brand_Apple',
+                                    'brand_Asus', 'brand_General', 'brand_Gionee', 'brand_Google',
+                                    'brand_HTC', 'brand_Honor', 'brand_Huawei', 'brand_InFocus',
+                                    'brand_LG', 'brand_Micromax', 'brand_Motorola', 'brand_Nokia',
+                                    'brand_OnePlus', 'brand_Oppo', 'brand_Panasonic', 'brand_Realme',
+                                    'brand_Samsung', 'brand_Vivo', 'brand_Xiaomi', 'ram_1', 'ram_2',
+                                    'ram_3', 'ram_4', 'ram_6', 'ram_8', 'ram_12', 'rom_1', 'rom_8',
+                                    'rom_16', 'rom_32', 'rom_64', 'rom_128', 'rom_256', 'rom_512'])
+        d = {'brand': brand, 'screen_size': screen, 'Primary_cam': Primary_cam, 'front_cam': front_cam,
+             'ram': ram, 'rom': rom}
+        data = pd.DataFrame(d, index=[1, 2])
+        F = pd.get_dummies(data, columns=['brand', 'ram', 'rom'])
+        new[F.columns.values] = F[F.columns.values]
+        new = new.replace(np.nan, 0)
+        x = new.iloc[:, :].values
+        pred=round(round(clf.predict(x)[0],2)/2)
+
+        return render_template('index.html', pred=pred, label=1)
+
+    except:
+
+        return render_template('index.html', label=0)
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
